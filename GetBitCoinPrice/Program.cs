9f09e57a897b8b810992c2f366c1b1ca5b08029a
DESKTOP-H5I8ON3\pguim <pguimalan@gmail.com>
2021-07-09 11:24:10 +0100

    Add project files.

---
+++ GetBitCoinPrice/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace GetBitCoinPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonStr;
+
+            using (var wc = new WebClient())
+            {
+                var url = @"https://api.coindesk.com/v1/bpi/currentprice.json";
+                jsonStr = wc.DownloadString(url);
+            }
+
+            dynamic obj = JsonConvert.DeserializeObject(jsonStr);
+            var currentPrice = obj.bpi.EUR.rate.Value;
+
+            Console.WriteLine($"BTC Price EUR {String.Format("{0:n}", currentPrice)}");
+        }
+    }
+}
